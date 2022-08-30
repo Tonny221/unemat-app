3b764a71495c294e3653870beaa9fe58b2c2c1b5
Tonny221 <anthonny14gabriell@gmail.com>
2022-08-30 16:20:53 -0400

    routes defined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Box } from '@mui/material';
 import './App.css';
+import Login from './pages/Login';
+import FormPage from './pages/FormPage'
+import IdCardPage from './pages/IdCardPage';
+import IdPage from './pages/IdPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Box>
-      Hello world
-    </Box>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/form' element={<FormPage />} />
+        <Route path='/id' element={<IdPage />} />
+        <Route path='/id-card' element={<IdCardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/IdCardPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IdCardPage = () => {
+  return (
+    <div>IdCardPage</div>
+  )
+}
+
+export default IdCardPage
