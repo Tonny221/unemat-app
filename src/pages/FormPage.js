4d47569971e1cef751120700b3dca64a45a0196b
Tonny221 <anthonny14gabriell@gmail.com>
2022-08-31 17:30:43 -0400

    form page start

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,8 +1,19 @@
+import { Box, Container, styled } from '@mui/material'
 import React from 'react'
+
+const SectionContainer = styled(Container)(({theme}) => ({
+  backgroundColor: '#D9D9D9',
+  height: '90vh',
+  borderRadius: '50% 50% 0 0'
+})); 
 
 const FormPage = () => {
   return (
-    <div>FormPage</div>
+    <Box sx={{ backgroundColor: '#038C33', minHeight: '100vh', paddingTop: '10rem' }}>
+      <SectionContainer>
+        
+      </SectionContainer>
+    </Box>
   )
 }
 
