8a3ba960ec52418500ce69ec71dd9f3ffe04a237
Tonny221 <anthonny14gabriell@gmail.com>
2022-08-31 14:06:45 -0400

    working on LogInBox

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,40 @@
-import { Box, Container, styled } from '@mui/material'
+import { Box, Container, styled, TextField } from '@mui/material'
 import React from 'react'
 import logoImg from '../images/logo-white.png'
 
+const Logo = styled(Box)(({ theme }) => ({
+  width: 'fit-content',
+  margin: '0 auto',
+  paddingTop: '4rem'
+}));
+
+const LogInBox = styled(Box)(({ theme }) => ({
+  backgroundColor: '#D9D9D9',
+  width: '100%',
+  height: '90vh',
+  borderRadius: '3rem',
+  boxShadow: '1px -5px 25px 10px rgba(0,0,0,0.5)',
+}));
+
+const LogInContainer = styled(Container)(({ theme }) => ({
+  backgroundColor: '#CDCDCD',
+  width: '80%',
+  textAlign: 'center'
+}));
+
 const Login = () => {
   return (
-    <Container sx={{ backgroundColor: '#251973', minHeight: '100vh'}}>
-        <Box sx={{width: 'fit-content', margin: '0 auto'}}>
-            <img src={logoImg} alt='logo' />
-        </Box>
-    </Container>
+    <Box sx={{ backgroundColor: '#251973', minHeight: '100vh' }}>
+      <Logo>
+        <img src={logoImg} alt='logo' />
+      </Logo>
+      <LogInBox paddingTop={'5rem'}>
+        <LogInContainer>
+          <TextField label='Email' placeholder='institucional@unemat.br' />
+          <TextField label='Senha' placeholder='senha (matricula)' />
+        </LogInContainer>
+      </LogInBox>
+    </Box>
   )
 }
 
