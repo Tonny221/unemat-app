f6a38321e40ce8eb499147cc8aa14d81866878c3
Tonny221 <anthonny14gabriell@gmail.com>
2022-09-01 16:18:03 -0400

    id card page start

--- src/pages/IdCardPage.js
+++ src/pages/IdCardPage.js
@@ -1,8 +1,36 @@
+import { Avatar, Box, styled } from '@mui/material'
 import React from 'react'
+
+const CardBox = styled(Box)(({ theme }) => ({
+  backgroundColor: '#038C33',
+  height: '34rem',
+  width: '22rem',
+  margin: '30% auto',
+  borderRadius: '4rem'
+}));
+
+const DataBox = styled(Box)(({ theme }) => ({
+
+}));
+
+const UserInfoCard = styled(Box)(({ theme }) => ({
+
+}));
+
+const UserAvatarCard = styled(Avatar)(({ theme }) => ({
+
+}));
 
 const IdCardPage = () => {
   return (
-    <div>IdCardPage</div>
+    <CardBox>
+      <DataBox>
+        <UserInfoCard>
+          <UserAvatarCard />
+          
+        </UserInfoCard>
+      </DataBox>
+    </CardBox>
   )
 }
 
